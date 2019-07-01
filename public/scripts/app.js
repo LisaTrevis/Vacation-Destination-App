abf5d769b0af40227d7e794b970e6f423757f3e7
Lisa Trevis <l_trevis@hotmail.com>
2019-06-30 19:54:01 -0700

    Added form that renders user input

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,27 +9,19 @@ console.log('app.js is running!');
 
 // NOTE: parens are NOT required, but they do make the code cleaner/easier to read.
 
-var templateTest = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'Vacation Destination App'
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Let\'s go!'
-    )
-);
+// const templateTest = (
+//     <div>
+//         <h1>Vacation Destination App</h1>
+//         <p>Let's go!</p>
+//     </div>
+// );
 
 // Here is some dynamic JSX pulling from an object:
 
 var app = {
     title: 'Vacation Destination App',
     subtitle: 'Let\'s go!',
-    options: ['Jamaica', 'Atlanta', 'Ghana'],
+    options: [],
     printOptions: function printOptions() {
         return this.options.map(function (option, index) {
             return React.createElement(
@@ -41,78 +33,64 @@ var app = {
     }
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options:' : 'No Options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        app.printOptions()
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
 
-var count = 0;
+    var option = e.target.elements.option.value;
 
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
-};
-var minusOne = function minusOne() {
-    count--;
-    renderCounterApp();
-};
-var reset = function reset() {
-    count = 0;
-    renderCounterApp();
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
 };
 
 var appRoot = document.getElementById('app');
 
-// .render takes two args: 1st is the JSX I want to render, 2nd is the DOM element in which I want to render the JSX.
-
-var renderCounterApp = function renderCounterApp() {
-    var templateTwo = React.createElement(
+var render = function render() {
+    var template = React.createElement(
         'div',
         null,
         React.createElement(
             'h1',
             null,
-            'Count: ',
-            count
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
         ),
         React.createElement(
-            'button',
-            { onClick: addOne },
-            '+1'
+            'p',
+            null,
+            app.options.length > 0 ? 'Here are your options:' : 'No Options'
         ),
         React.createElement(
-            'button',
-            { onClick: minusOne },
-            '-1'
+            'p',
+            null,
+            app.options.length
         ),
         React.createElement(
-            'button',
-            { onClick: reset },
-            'Reset'
+            'ol',
+            null,
+            app.printOptions()
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add option'
+            )
         )
     );
 
-    ReactDOM.render(templateTwo, appRoot);
+    // .render takes two args: 1st is the JSX I want to render, 2nd is the DOM element in which I want to render the JSX.
+
+    ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+render();
--- src/app.js
+++ src/app.js
@@ -7,65 +7,58 @@ console.log('app.js is running!');
 
 // NOTE: parens are NOT required, but they do make the code cleaner/easier to read.
 
-const templateTest = (
-    <div>
-        <h1>Vacation Destination App</h1>
-        <p>Let's go!</p>
-    </div>
-);
+// const templateTest = (
+//     <div>
+//         <h1>Vacation Destination App</h1>
+//         <p>Let's go!</p>
+//     </div>
+// );
 
 // Here is some dynamic JSX pulling from an object:
 
 const app = {
     title: 'Vacation Destination App',
     subtitle: 'Let\'s go!',
-    options: ['Jamaica', 'Atlanta', 'Ghana'],
+    options: [],
     printOptions() {
         return this.options.map((option, index) => (<li key={index}>{option}</li>));
     }
 };
 
-const template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options:' : 'No Options'}</p>
-        <ol>
-            {app.printOptions()}
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+    e.preventDefault();
 
-let count = 0;
+    const option = e.target.elements.option.value;
 
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-};
-const reset = () => {
-    count = 0;
-    renderCounterApp();
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        render();
+    }
 };
 
 const appRoot = document.getElementById('app');
 
-// .render takes two args: 1st is the JSX I want to render, 2nd is the DOM element in which I want to render the JSX.
-
-const renderCounterApp = () => {
-    const templateTwo = (
+const render = () => {
+    const template = (
         <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>Reset</button>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.options.length > 0 ? 'Here are your options:' : 'No Options'}</p>
+            <p>{app.options.length}</p>
+            <ol>
+                {app.printOptions()}
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option"/>
+                <button>Add option</button>
+            </form>
         </div>
     );
+    
+    // .render takes two args: 1st is the JSX I want to render, 2nd is the DOM element in which I want to render the JSX.
 
-    ReactDOM.render(templateTwo, appRoot);
-};
- 
-renderCounterApp();
+    ReactDOM.render(template, appRoot);
+}
+
+render();
