def3eb051cceec3a218a36b32de87eb4fef9a326
Lisa Trevis <l_trevis@hotmail.com>
2019-07-02 19:52:00 -0700

    addedd demo class and two instances

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,38 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Person = function Person(name) {
-    _classCallCheck(this, Person);
-};
+var Person = function () {
+    // constructor is implicitly run when a new instance of Person is created.
+    function Person() {
+        var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Anonymous';
 
-var me = new Person('Lisa Trevis');
+        _classCallCheck(this, Person);
+
+        // this provides a default value if one isn't provided when the class constructor is called.
+        this.name = name;
+    }
+    // getGreeting must be explicity called in order to run.
+
+
+    _createClass(Person, [{
+        key: 'getGreeting',
+        value: function getGreeting() {
+            // return 'Hi, ' + this.name + '!';
+            // We can interpolate by using backticks, to avoid concatenation and whitespace errors:
+            return 'Hi, ' + this.name + '!';
+        }
+    }]);
+
+    return Person;
+}();
+
+var me = new Person('Lisa Trevis'); // Results in Person { name: Lisa Trevis }
 console.log(me);
+
+var other = new Person(); // Results in Person { name: Anonymous }
+console.log(other);
+
+console.log(me.getGreeting());
--- src/playground/classes.js
+++ src/playground/classes.js
@@ -1,8 +1,20 @@
 class Person {
-    constructor(name) {
-
+    // constructor is implicitly run when a new instance of Person is created.
+    constructor(name = 'Anonymous') { // this provides a default value if one isn't provided when the class constructor is called.
+        this.name = name;
+    }
+    // getGreeting must be explicity called in order to run.
+    getGreeting() {
+        // return 'Hi, ' + this.name + '!';
+        // We can interpolate by using backticks, to avoid concatenation and whitespace errors:
+        return `Hi, ${ this.name }!`
     }
 }
 
-const me = new Person('Lisa Trevis');
+const me = new Person('Lisa Trevis'); // Results in Person { name: Lisa Trevis }
 console.log(me);
+
+const other = new Person(); // Results in Person { name: Anonymous }
+console.log(other);
+
+console.log(me.getGreeting());
