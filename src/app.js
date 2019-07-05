60d7e8a9fbcede87d164adabf09e46ed09c303dc
Lisa Trevis <l_trevis@hotmail.com>
2019-07-04 17:37:20 -0700

    added methods to buttons

--- src/app.js
+++ src/app.js
@@ -27,19 +27,28 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+    handlePick() {
+        alert('handlePick');
+    }
+    
     render() {
         return (
             <div>
-                <button>Where should I go?</button>
+                <button onClick={this.handlePick}>Where should I go?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
+    handleRemoveAll() {
+        alert('Done');
+    }
+    
     render() {
         return (
             <div>
+                <button onClick={this.handleRemoveAll}>Remove All</button>
                 {
                     this.props.options.map((option) => <Option key={option} optionText={option} />)
                 }
@@ -59,14 +68,28 @@ class Option extends React.Component {
     }
 }
     
-    class AddOption extends React.Component {
+class AddOption extends React.Component {
+    handleAddOption(e) {
+        e.preventDefault();
+
+        const option = e.target.elements.option.value.trim();
+        
+        if(option) {
+            alert(option);
+        }
+    };
+
     render() {
-        return (
-            <div>
-                AddOption component here
-            </div>
+    return (
+        <div>
+            <form onSubmit={this.handleAddOption}>
+                <input type="text" name="option" />
+                <button>Add option</button>
+            </form>
+        </div>
         );
     }
 }
+
 
 ReactDOM.render(<VacationDestinationApp />, document.getElementById('app'));
